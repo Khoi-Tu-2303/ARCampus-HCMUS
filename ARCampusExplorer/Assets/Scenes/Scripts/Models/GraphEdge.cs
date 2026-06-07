542f7dd156e31f4ae2e5c655552449b228ecec64
Khoi Tu <tuvankhoi123@gmail.com>
2026-06-07 20:52:31 +0700

    Remove old ARCampusExplorer directory

--- ARCampusExplorer/Assets/Scenes/Scripts/Models/GraphEdge.cs
+++
@@ -1,9 +0,0 @@
-// Models/GraphEdge.cs
-[System.Serializable]
-public class GraphEdge
-{
-    public string from;
-    public string to;
-    public float distance_m;
-    public float direction_deg;
-}
